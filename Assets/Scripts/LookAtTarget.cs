09dfc6f7c341664943576459c664ef2a7ef0db00
Thomas <Kniff1@hotmail.de>
2020-11-08 11:01:30 +0100

    Add basic movement and camera scripts (Refactor)

---
+++ Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour
+{
+    [SerializeField] private GameObject _target;
+
+    void LateUpdate()
+    {
+        transform.LookAt(_target.transform);
+    }
+}
